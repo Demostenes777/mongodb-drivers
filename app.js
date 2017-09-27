b74848a4bc4ed27606e34b1bd2c20ba02ad75733
Demostenes777 <alaitz.mendiola@gmail.com>
2017-09-27 14:08:25 +0200

    Project setup

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+// Watch for incoming requests of method get
+// to the route http://localhost:3050/api
+
+app.get('/api', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+
+module.exports = app;
