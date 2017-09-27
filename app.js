d646de43dbc0eccec65a0370f65fd7c82460bc12
Demostenes777 <alaitz.mendiola@gmail.com>
2017-09-27 17:30:01 +0200

    Implementation and testing of a driver creation

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const routes = require('./routes/routes');
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/drivers');
+
+app.use(bodyParser.json());
 routes(app);
 
 module.exports = app;
